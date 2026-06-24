2e7bbee00b753ff2135912c19d39e43d8d158049
twinklechhillar <twinklechhillar89@gmail.com>
2026-06-24 08:17:29 +0530

    Create reverse.java

---
+++ reverse.java
@@ -0,0 +1,9 @@
+class Main {
+    public static void main(String[] args) {
+        String s = "hello";
+        for(int i=s.length()-1; i>=0; i--){
+         System.out.println(s.charAt(i));    
+        }
+       
+    }
+}
