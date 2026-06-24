e952e019c064b6a33b8619921dcc48e772611ab8
twinklechhillar <twinklechhillar89@gmail.com>
2026-06-24 07:50:48 +0530

    Create basic.java

---
+++ basic.java
@@ -0,0 +1,9 @@
+class Main {
+    public static void main(String[] args) {
+        String s ="hello";
+        
+        System.out.println("length=" +s.length()  );
+        System.out.println("first character=" +s.charAt(0)  );
+        System.out.println("last character=" + s.charAt(4)  );
+    }
+}
