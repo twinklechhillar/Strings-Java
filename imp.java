5174e7a3f7ebf03ad242037097994fb338703dfe
twinklechhillar <twinklechhillar89@gmail.com>
2026-06-24 08:05:11 +0530

    Create imp.java

---
+++ imp.java
@@ -0,0 +1,19 @@
+class Main {
+    public static void main(String[] args) {
+        String s = "hello";
+        for(int i=0; i <s.length(); i++){
+            System.out.print(s.charAt(i));
+        }
+        
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        String s = "hello";
+        for(int i=0; i <s.length(); i++){
+            System.out.print(s.charAt(i) +" ");
+        }
+        
+    }
+}
