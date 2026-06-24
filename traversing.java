37a022f9555cfa46676c805e8f7f1dd527a19c6a
twinklechhillar <twinklechhillar89@gmail.com>
2026-06-24 07:57:47 +0530

    Create traversing.java

---
+++ traversing.java
@@ -0,0 +1,9 @@
+class Main {
+    public static void main(String[] args) {
+        String s = "hello";
+        for(int i=0; i <s.length(); i++){
+            System.out.println(s.charAt(i));
+        }
+        
+    }
+}
