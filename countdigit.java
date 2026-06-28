579609caa5dcd1a19b72f421d5bd2f7c22d5628d
twinklechhillar <twinklechhillar89@gmail.com>
2026-06-28 11:50:33 +0530

    Create countdigit.java

---
+++ countdigit.java
@@ -0,0 +1,15 @@
+public static void main(String[] args) {
+        String s = "abc12345";
+        int count =0;
+        for(int i=0; i<s.length(); i++){
+            char ch = s.charAt(i);
+            
+            if(ch>='0' && ch<='9'){
+                count++;
+            }
+            
+        }
+        
+        System.out.println(count);
+    }
+}
