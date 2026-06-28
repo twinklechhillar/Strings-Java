1705003219a201f128f32c04ffb25e5bf7124efb
twinklechhillar <twinklechhillar89@gmail.com>
2026-06-28 12:20:38 +0530

    Create replace.java

---
+++ replace.java
@@ -0,0 +1,14 @@
+class Main {
+    public static void main(String[] args) {
+        String s = "banana";
+        for(int i=0; i<s.length(); i++){
+            char ch = s.charAt(i);
+            if(ch =='a'){
+             System.out.println('o');   
+            } else{
+             System.out.print(ch);     
+            }
+        }
+        
+    }
+}
